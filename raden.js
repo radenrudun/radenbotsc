b918e6ee1711e1922917df8e9639fdada84a85ad
radenrudun <radennoven@gmail.com>
2025-05-30 16:53:53 +0700

    Add files via upload

--- raden.js
+++ raden.js
@@ -385,29 +385,25 @@ ${daftarKey}
         break
         case 'help':
 case 'bantuan': {
-  const sections = [{
-    title: "📌 PILIHAN CEPAT",
-    rows: [
-      { title: "📄 Lihat Produk", rowId: "list" },
-      { title: "📞 Kontak Owner", rowId: "owner" }
-    ]
-  }]
-
-  const listMessage = {
+  await raden.sendMessage(from, {
     text: `*💡 PANDUAN CARA PENGGUNAAN*\n\n` +
-          `> Ketik \`list\` dan kirim untuk menampilkan produk\n` +
-          `> Dalam list ada key seperti: \`FREE FIRE\`, Jika anda mengetik dan mengirim \`FREE FIRE\` maka akan otomatis muncul harganya.\n` +
-          `> Info lebih lanjut bisa ketik \`owner\`.\n` +
-          `> Untuk info update silahkan join grup:\n` +
+          `> Ketik \`list\` untuk menampilkan produk.\n` +
+          `> Jika anda sudah mengirim \`list\`, maka akan ada respon otomatis yang menampilkan list produk.\n` +
+          `> Didalam \`list\` ada berupa key contoh \`FREE FIRE\`, dan jika anda memasukkan key tersebut maka akan menampilkan harga dari produk itu.\n` +
+          `> Untuk info lebih lanjut bisa ketik \`owner\`, atau join grup untuk info update:\n` +
           `> \`\`\`https://chat.whatsapp.com/GPQXetga5XpKr4dih7a6Z8\`\`\``,
-    footer: `${botName} • ${botVersion}`,
-    buttonText: "🔘 MENU CEPAT",
-    sections
-  }
-
-  await raden.sendMessage(from, listMessage, { quoted: m });
+    contextInfo: {
+      externalAdReply: {
+        title: '📢 RADEN STORE - TOPUP MURAH',
+        body: 'Klik untuk masuk grup WhatsApp',
+        sourceUrl: 'https://chat.whatsapp.com/GPQXetga5XpKr4dih7a6Z8',
+        mediaType: 1,
+        showAdAttribution: true
+      }
+    }
+  }, { quoted: m });
 }
-break
+break;
         //ALLMENU RADEN STORE
 case 'raden':
   case 'menu': {
